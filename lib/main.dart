d1242d65fc099afd32786a56435580aa4940850e
parth13075 <109304629+parth13075@users.noreply.github.com>
2024-02-09 19:39:34 +0000

    added comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,31 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:test_app2/firebase_options.dart';
 
-void main() async{
+///This is the main function, It is called at the very start
+void main() async{ 
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
+        options: DefaultFirebaseOptions.currentPlatform,    //Connect to firebase
     );
-    runApp(const MyApp());
+    runApp(const MyApp());  //The main app is run.
 }
 
+///This is the main app class. It is a stateless widget as there are no widget insider THIS class
+///which is changed frequently
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  @override                                 //Override any inherited methods
+  Widget build(BuildContext context) {      //Main build function
+    return const MaterialApp(               //Return a MaterialApp
       home: Home()
     );
   }
 }
 
-
-class OpenPage extends StatefulWidget {
-    final String societyName;
+///This is the class for the page when the user clicks on a society card to open it.
+class OpenPage extends StatefulWidget {     
+    final String societyName;                   //It takes 1 parameter, the name of the society
     const OpenPage(this.societyName, {super.key});
 
     @override
@@ -33,31 +36,31 @@ class OpenPage extends StatefulWidget {
 class _OpenPageState extends State<OpenPage> {
     @override
     Widget build(BuildContext context) {
-        return Scaffold(
+        return Scaffold(                                //Create a scafold app.
             backgroundColor: Colors.grey.shade900,
-            appBar: AppBar(
+            appBar: AppBar(                             //Create appBar and style it
                 title: Text(
                     widget.societyName,
                     style: const TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'ProtestStrike'
+                        color: Colors.white,    
+                        fontFamily: 'ProtestStrike'     //Use font ProtestStike, declared in pubspec.yaml
                     ),
                 ),
-                centerTitle:true,
+                centerTitle:true,                   
                 backgroundColor: Colors.grey.shade700,
             ),
 
-            body: Container(
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+            body: Container(                            //Enclose all widgets in the body with a Container Widget
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 5), //Add some margin
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                        TextField(
+                    children: [     
+                        TextField(                      //Create an input text field
                             style: const TextStyle(
                                 fontFamily: 'ProtestStrike',
                                 fontSize: 18
                             ),
-                            decoration: InputDecoration(
+                            decoration: InputDecoration(      //Style the input text field
                                 filled: true,
                                 hintText: 'Chat here...',
                                 hintStyle: const TextStyle(
@@ -79,8 +82,8 @@ class _OpenPageState extends State<OpenPage> {
     }
 }
 
-
-class Home extends StatefulWidget {
+///This is the class for the Home page of the app
+class Home extends StatefulWidget {                 //This is the class which 
     const Home({super.key});
 
   @override
@@ -89,27 +92,27 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
     
-    List<Widget> societyCards = []; //List of society cards
+    List<Widget> societyCards = [];                     //Create a list of societyCards
 
-    late TextEditingController controller;
+    late TextEditingController controller;              //Controllers for textInput in the AlertDialog
     late TextEditingController descriptionController;
 
     @override
-    void initState(){
+    void initState(){                                   //Initialise controllers
         super.initState();
         controller = TextEditingController();
         descriptionController = TextEditingController();
     }
 
     @override
-    void dispose(){
+    void dispose(){                                     //Method to dispose the controllers.
         controller.dispose();
         descriptionController.dispose();
         super.dispose();
     }
 
-    Widget createSocietyCard(name, description){
-        Navigator.of(context).pop();
+    Widget createSocietyCard(name, description){       //Method which returns a Card Widget to create for a society. It takes the name and description as parameters
+        Navigator.of(context).pop();                  
         controller.clear();
         descriptionController.clear();
         return Card(
@@ -123,7 +126,7 @@ class _HomeState extends State<Home> {
                     Padding(
                     padding: const EdgeInsets.all(30.0),
                     child: Column(
-                      children: [
+                        children: [
                         Center(
                             child: Text(
                                 '$name',
@@ -141,21 +144,19 @@ class _HomeState extends State<Home> {
                                 style: TextStyle(
                                     fontFamily: 'ProtestStrike',
                                     color: Colors.grey.shade200
-                
                                 ),
-
                             ) ,
                         )
-                      ],
+                        ],
                     ),
-                    ),
+                ),
 
-                    ElevatedButton(
+                    ElevatedButton(             //Create a button in every card to open its own page.
                         onPressed: () {
-                            Navigator.push(
+                            Navigator.push(     //If OPEN button clicked, open its page and add to stack to retain pages opened in order
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => OpenPage(name)
+                                    builder: (context) => OpenPage(name)   //Create instance of OpenPage class.
                                 )
                             );
                         },
@@ -177,12 +178,12 @@ class _HomeState extends State<Home> {
         );
     }
 
-    Future openTextInput() => showDialog(
+    Future openTextInput() => showDialog(       //This method opens the input dialog to ask for name and description of society
         context: context,
         builder: (context) => Center(
           child: Wrap(
             children: [
-                    AlertDialog(
+                    AlertDialog(                            //Create AlertDialog Widget
                     title: const Text('Your Society Name'),
                     content: Column(
                     children: [
@@ -207,7 +208,7 @@ class _HomeState extends State<Home> {
                         TextButton(
                             onPressed:() => {
                                 setState(() {
-                                    societyCards.add(createSocietyCard(controller.text, descriptionController.text));
+                                    societyCards.add(createSocietyCard(controller.text, descriptionController.text));   //If Submit button pressed, create new card instance, and add to societyCards list.
                                 })
                             },
                             child: const Text('SUBMIT'),
@@ -220,10 +221,10 @@ class _HomeState extends State<Home> {
     );
 
     @override
-    Widget build(BuildContext context) {
-        return Scaffold(
+    Widget build(BuildContext context) {        //This is the first build method of the Home Page
+        return Scaffold(    
             backgroundColor: Colors.grey.shade900,
-            appBar: AppBar(
+            appBar: AppBar(                             //Create appBar and style it.
                 title: const Text(
                     'Add a new society',
                     style: TextStyle(
@@ -258,16 +259,16 @@ class _HomeState extends State<Home> {
                         const SizedBox(
                             height: 6,
                         ),
-                        ...societyCards,
+                        ...societyCards,        //Load the societies in the societyList here
 
                     ],
                 ),
             ),
 
-            floatingActionButton: FloatingActionButton(
+            floatingActionButton: FloatingActionButton(     //Create a floating button in bottom left to be able to add a society
                 backgroundColor: Colors.grey.shade300,
                 onPressed: () => {
-                    openTextInput()
+                    openTextInput()                         //Call openTextInput function. 
                 },
                 child: const Text('+',
                     style: TextStyle(fontSize: 25.0),
